f6662d65c1f00b94a61ac506a433f7ce65627060
shreyashsingrupkar <shreyashsingrupkar@gmail.com>
2024-02-26 23:47:41 +0530

    smart school application

---
+++ src/main/java/com/ietch/smartschools/SmartschoolsApplication.java
@@ -0,0 +1,13 @@
+package com.ietch.smartschools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartschoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartschoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ietch/smartschools/SmartschoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ietch.smartschools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartschoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
